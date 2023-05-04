234ad780e949a5d5aa5ef053ab68e274010f5796
pedrolez <50318819+pedrolez@users.noreply.github.com>
2023-05-04 21:14:05 +0200

    Add files via upload

---
+++ Ejercicios 11.1/Ejercicio2/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+
+    public delegate void Infinitvo();
+
+    static void Ser()
+    {
+        Console.WriteLine("To be");
+    }
+
+    static void Correr()
+    {
+        Console.WriteLine("Run");
+    }
+
+    static void Ver()
+    {
+        Console.WriteLine("See");
+    }
+
+    static void Pensar()
+    {
+        Console.WriteLine("Think");
+    }
+
+      static void Comer()
+    {
+        Console.WriteLine("Eat");
+    }
+    private static void Main(string[] args)
+    {
+        Infinitvo inf = null;
+
+        inf += Ser;
+        inf += Correr;
+        inf += Ver;
+        inf();
+
+        inf -= Ser;
+        inf -= Ver;
+        inf += Pensar;
+        inf += Comer;
+        inf();
+
+    }
+}
